282aa7df0b9e6dd168d4a28ebad2efde65872424
qmertesdorf <quintenmertesdorf@gmail.com>
2019-04-01 15:45:32 -0700

    add function to check for daily posts

--- index.js
+++ index.js
@@ -7,22 +7,40 @@ const client = new Discord.Client();
 
 const app = express();
 
+let CHANNELS;
+
 client.on('ready', () => {
   console.log('Bot is up and ready!');
+  CHANNELS = Object.values(users).map((obj) => {
+    return client.channels.get(obj.channelID);
+  })
+  // console.log(CHANNELS);
+  console.log(checkForDailyPost());
 });
 
-client.on('message', (message) => {
-  // console.log(message.channel.id);
-  // console.log(message.channel.lastMessage.author.id);
-  // console.log(message.channel.lastMessage.author.username);
-  // console.log(message.channel.lastMessage.content);
-  const messageData = {
-    userID: message.channel.lastMessage.author.id,
-    channelID: message.channel.id
-  }
+// client.on('message', (message) => {
+//   // console.log(message.createdTimestamp);
+//   // console.log(message.channel.lastMessage.createdTimestamp);
+//   // console.log(message.author.id);
+//   // console.log(message.author.username);
+//   // console.log(message.content);
+//   const messageData = {
+//     userID: message.author.id,
+//     channelID: message.channel.id
+//   }
 
-  console.log(getKeyFromValue(users, messageData));
-})
+//   // console.log(getKeyFromValue(users, messageData));
+// })
+
+// client.setInterval(())
+
+const checkForDailyPost = () => {
+  const matchingPosts = CHANNELS.filter(channel => {
+    //TODO - look into message.array instead of message.map(e => e)
+    return mostRecentMessage = channel.fetchMessages({limit: 1}).then(message => Date.now() - message.map(e => e)[0].createdTimestamp);
+  })
+  return matchingPosts
+}
 
 const getKeyFromValue = (obj, val) => {
   return Object.keys(obj).find((key) => {
