6573cd0e16c55c944e518e88902ebb6cf45e2b20
qmertesdorf <quintenmertesdorf@gmail.com>
2019-04-01 10:10:13 -0700

    add start up functionality

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const express = require("express");
 const client = new Discord.Client();
 
+const app = express();
+
+client.on('ready', () => {
+  console.log('Bot is up and ready!');
+})
+
+
+
+const config = {
+  port: process.env.PORT || "8080"
+};
+
+app.listen(config.port, () => {
+  console.log("App listening on", config.port);
+});
