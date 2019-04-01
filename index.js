138eaf2e706e746c7f53cd263a113028f944d6d2
qmertesdorf <quintenmertesdorf@gmail.com>
2019-04-01 13:40:06 -0700

    implement object comparison for future user targeting

--- index.js
+++ index.js
@@ -7,17 +7,34 @@ const client = new Discord.Client();
 
 const app = express();
 
-
-
 client.on('ready', () => {
   console.log('Bot is up and ready!');
 });
+
+client.on('message', (message) => {
+  // console.log(message.channel.id);
+  // console.log(message.channel.lastMessage.author.id);
+  // console.log(message.channel.lastMessage.author.username);
+  // console.log(message.channel.lastMessage.content);
+  const messageData = {
+    userID: message.channel.lastMessage.author.id,
+    channelID: message.channel.id
+  }
+
+  console.log(getKeyFromValue(users, messageData));
+})
+
+const getKeyFromValue = (obj, val) => {
+  return Object.keys(obj).find((key) => {
+    return Object.values(obj[key])[0] === Object.values(val)[0] && Object.values(obj[key])[1] === Object.values(val)[1];
+  })
+}
+
 
 const config = {
   port: process.env.PORT || "8080"
 };
 
-console.log(users);
 const token = tokenJSON.token;
 client.login(token);
 
