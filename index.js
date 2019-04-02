5daa327d819c0472f6ddf4fdb0a62939bc536507
qmertesdorf <quintenmertesdorf@gmail.com>
2019-04-02 16:57:36 -0700

    clean up previous code implementations, and add bot ability to post to channel

--- index.js
+++ index.js
@@ -1,59 +1,61 @@
 const users = require("./users.json");
 const express = require("express");
 const Discord = require("discord.js");
-const tokenJSON = require('./auth.json');
-const schedule = require('node-schedule');
+const tokenJSON = require("./auth.json");
 const client = new Discord.Client();
 
 const app = express();
-
 let CHANNELS;
 
-client.on('ready', () => {
-  console.log('Bot is up and ready!');
-  CHANNELS = Object.values(users).map((obj) => {
+client.on("ready", () => {
+  console.log("Bot is up and ready!");
+  CHANNELS = Object.values(users).map(obj => {
     return client.channels.get(obj.channelID);
-  })
-  // console.log(CHANNELS);
-  console.log(checkForDailyPost());
+  });
+  checkForDailyPost();
 });
 
-// client.on('message', (message) => {
-//   // console.log(message.createdTimestamp);
-//   // console.log(message.channel.lastMessage.createdTimestamp);
-//   // console.log(message.author.id);
-//   // console.log(message.author.username);
-//   // console.log(message.content);
-//   const messageData = {
-//     userID: message.author.id,
-//     channelID: message.channel.id
-//   }
+client.on('message', (message) => {
+  console.log(getUserFromMessageData(message));
+})
 
-//   // console.log(getKeyFromValue(users, messageData));
-// })
+//better describe what this does
+const getUserFromMessageData = message => {
+  return Object.keys(users).find(user => {
+    const userData = users[user];
+    return (
+      userData.userID === message.author.id &&
+      userData.channelID === message.channel.id
+    );
+  });
+};
 
-// client.setInterval(())
+//Check to see if any user has not posted within designated time limit every 5 minutes
+client.setInterval(checkForDailyPost, 300 * 1000)
+
+
 
 const checkForDailyPost = () => {
-  const matchingPosts = Promise.all(CHANNELS.map(channel => {
-    //TODO - look into message.array instead of message.map(e => e)
-    // channel.fetchMessages({limit: 1}).then(message => (Date.now() - message.map(e => e)[0].createdTimestamp > 8640));
-    let messageIsValid;
-    const channels = channel.fetchMessages({limit: 1}).then(message => {
-      messageIsValid = Date.now() - message.map(e => e)[0].createdTimestamp > 8640000
+  const messageQueue = Promise.all(
+    CHANNELS.map(channel => {
+      return channel.fetchMessages({ limit: 1 });
+    })
+  );
+  messageQueue.then(messages => {
+    //convert the messages array from an array of Discord.js 'Collections' to an array of objects
+    const formattedMessages = messages.map((message) => message.array()[0])
+    //Filter out any messages that have been created recently
+    const validMessages = formattedMessages.filter(message =>  {
+      const user = getUserFromMessageData(message);
+      //check that the user's message was the last message in the channel - this is to prevent the bot from spamming them everytime this runs
+      return (user && Date.now() - message.createdTimestamp > 86400000);
     });
-    return messageIsValid;
-  }))
+    validMessages.map(message => {
+      message.channel.send(`Hey ${message.author}, it looks like you haven't posted in a while. We'd love to hear how you're doing so far!`);
+    })
+  });
+};
 
-  return matchingPosts;
-}
-
-const getKeyFromValue = (obj, val) => {
-  //Clean up and make more specific
-  return Object.keys(obj).find((key) => {
-    return Object.values(obj[key])[0] === Object.values(val)[0] && Object.values(obj[key])[1] === Object.values(val)[1];
-  })
-}
 
 
 const config = {
